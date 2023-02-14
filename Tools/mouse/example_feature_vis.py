21ddfc410cbd58cfba37bf850c9e0198579cd1da
runhey <2234044577@qq.com>
2023-02-14 11:42:44 +0800

    feat(Tools):example of robot-mouse-track

---
+++ Tools/mouse/example_feature_vis.py
@@ -0,0 +1,35 @@
+from trace_examples import  trace_answer_unlock
+from robot_mouse_track.mouse_track import MouseTrack
+
+import matplotlib.pyplot as plt
+
+# if __name__ == '__main__':
+mouse_track = MouseTrack(trace_answer_unlock)
+
+# 展示鼠标轨迹
+mouse_track.show_track()
+
+# 展示合速度
+feature_dev_order1, feature_dev_order2 = mouse_track.get_feature_dev(order=2, mode="combine")  # contants.COMBINE
+plt.plot(mouse_track.arr_trace[:len(feature_dev_order1),-1], feature_dev_order1)
+plt.xlabel("time")
+plt.ylabel("px/ms")
+plt.show()
+#
+# plt.plot(mouse_track.arr_trace[:len(feature_dev_order2),-1], feature_dev_order2)
+# plt.xlabel("time")
+# plt.ylabel("px/ms^2")
+# plt.show()
+
+# 展示分速度
+# feature_dev_order1, feature_dev_order2 = mouse_track.get_feature_dev(order=2, mode="decomposition") # contants.COMBINE
+# plt.plot(mouse_track.arr_trace[:len(feature_dev_order1), -1], feature_dev_order1[:, 0])
+# plt.xlabel("time")
+# plt.ylabel("x-axis px/ms")
+# plt.show()
+
+feature_doa = mouse_track.get_feature_doa()
+plt.plot(mouse_track.arr_trace[:len(feature_doa), -1], feature_doa)
+plt.xlabel("time")
+plt.ylabel("angle")
+plt.show()
---
+++ Tools/mouse/example_motion_jump.py
@@ -0,0 +1,11 @@
+from trace_examples import trace_selenium_jump
+from robot_mouse_track.mouse_track import MouseTrack
+from robot_mouse_track.risk_motion.motion_jump import JumpMotion
+
+if __name__ == '__main__':
+    mouse_track = MouseTrack(trace_selenium_jump)
+    mouse_track.show_track()
+    rule_mouse_jump = JumpMotion(th_velocity=20, th_acceleration=1.4)
+    flag, (exceed_times_velocity, exceed_times_acceleration) = rule_mouse_jump.judge_risk(mouse_track)
+    print(flag,exceed_times_velocity,exceed_times_acceleration)
+
